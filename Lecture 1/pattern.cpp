19425c24e11f5f006d960e11be08fb0edfb7b664
Harshit Sharma <121124691+harshit-sharmaaa@users.noreply.github.com>
2023-06-21 22:12:49 +0530

    Update pattern.cpp

--- Lecture 1/pattern.cpp
+++ Lecture 1/pattern.cpp
@@ -1,22 +1,32 @@
 #include <iostream>
 using namespace std;
 
+//Pattern-1
+// 1
+// 23
+// 456
+// 78910
+// 1112131415
+
 int main() {
 
-    int n,i=1,o=1;
-    cin >> n;
-
-    while(i<=n){
-        int t=1;
-        while(t<=i){
-            cout << o;
-            o++;
+    int n;
+    cout << "Enter rows : " << endl;
+    cin >> n ;
+    int row=1;
+    int t=1;
+    while (row <= n){
+        int start=1;
+        while (start <= row){
+            cout << t ;
+            start++;
             t++;
         }
-        
-        i++;
+
+        row++;
         cout << endl;
     }
+    
     
 
     return 0;
