24f5b907c66b70f2071aede0f326c768d225e9e4
Harshit Sharma <121124691+harshit-sharmaaa@users.noreply.github.com>
2023-06-23 14:02:30 +0530

    Update printPrime.cpp

---
+++ Lecture 1 HW/printPrime.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter a number";
+    cin >> n;
+    int i=2;
+    while (i<=n){
+        int t=2;
+        int prime=1;
+        while (t<i){
+            if (i%t==0){
+                prime=0;
+                break;               
+            }            
+            t++;
+        }
+        if (prime){
+            cout << i;
+        }        
+        i++;
+        cout << " ";
+    }
+    return 0;
+}
